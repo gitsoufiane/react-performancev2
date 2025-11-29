5ed602b71ec2e0e39b879dfb2cd1e69d1837f059
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2025-11-29 09:21:19 -0800

    Update script source in index.html and refactor Application and Pokemon components for improved performance

--- src/examples/pokamoka/application.tsx
+++ src/examples/pokamoka/application.tsx
@@ -1,13 +1,26 @@
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useTransition } from 'react';
 
 import { Container } from '$components/container';
 import { Input } from '$components/input';
-import { Pokemon } from './components/pokemon';
+import { MemoizedPokemon } from './components/pokemon';
 import { filterPokemon } from './utilities/filter-pokemon';
 
 const Application = () => {
+  const [inputQuery, setInputQuery] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
+  const [_isPending, startTransition] = useTransition();
+
   const filteredPokemon = useMemo(() => filterPokemon(searchQuery), [searchQuery]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setInputQuery(newValue);
+    startTransition(() => {
+      setSearchQuery(newValue);
+    });
+  }
+
+
 
   return (
     <Container className="space-y-8">
@@ -15,13 +28,13 @@ const Application = () => {
         <Input
           label="Search Pokemon"
           placeholder="Search by name, type, ability, species, or description…"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
+          value={inputQuery}
+          onChange={handleInputChange}
         />
       </section>
       <section className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
         {filteredPokemon.map((pokemon) => (
-          <Pokemon key={pokemon.id} {...pokemon} />
+          <MemoizedPokemon key={pokemon.id} {...pokemon} />
         ))}
       </section>
     </Container>
--- src/examples/pokamoka/components/pokemon.tsx
+++ src/examples/pokamoka/components/pokemon.tsx
@@ -2,6 +2,7 @@ import { Badge } from '$components/badge';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '$components/card';
 import { Progress } from '$components/progress';
 import { Tag } from '$components/tag';
+import { memo } from 'react';
 import type Pokedex from '../pokedex.json';
 
 type PokemonRecord = (typeof Pokedex)[number];
@@ -27,7 +28,7 @@ const typeColors: Record<string, 'neutral' | 'success' | 'warning' | 'error' | '
   Steel: 'neutral',
 };
 
-export const Pokemon = ({
+const Pokemon = ({
   id,
   name,
   type,
@@ -110,3 +111,5 @@ export const Pokemon = ({
     </Card>
   );
 };
+
+export const MemoizedPokemon = memo(Pokemon);
